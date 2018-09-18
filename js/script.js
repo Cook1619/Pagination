9a8bfd98eb3d7e95a78b63467742b82d8c39cb30
Cook1619 <cook6348@gmail.com>
2018-09-17 19:41:45 -0500

    Hiding all student list and logging to the console my list of students to see what returns

--- js/script.js
+++ js/script.js
@@ -4,17 +4,20 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
-let student = $(".student-item");
-
-for (let i = 0; i < student.length; i++) {
-  console.log(student[i]);
-}
+const student = $(".student-item");
+const studentList = $(".student.list");
+const pages = null;
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-let showPage = () => {};
-
+const showPage = () => {
+  student.hide();
+  for (let i = 0; i < student.length; i++) {
+    console.log(student[i]);
+  }
+};
+showPage();
 // Create and append the pagination links - Creating a function that can do this is a good approach
-let appendPageLinks = () => {};
+const appendPageLinks = () => {};
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
