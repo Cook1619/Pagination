fa16c98ee3405d66f8fc600a8e4a791fb528a881
Cook1619 <cook6348@gmail.com>
2018-09-23 18:50:06 -0500

    Adding page numbers and working through logic to show what I want on the main page

--- js/script.js
+++ js/script.js
@@ -5,20 +5,25 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const $student = $(".student-item");
-const list = $(".student.list");
-console.log($student);
+const $list = $(".student.list");
+let pageNumber = 1;
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 const showPage = (page, student) => {
-  for (let i = 0; i <= student.length; i++) {
-    if (student[i] >= student[0] && student[i] <= student[9]) {
+  const maxPage = page * 10 - 1;
+  const minPage = page * 10 - 10;
+  for (let i = 0; i < student.length; i++) {
+    student.hide();
+    if (i >= minPage && i <= maxPage) {
       student.show();
-    } else {
-      student.hide();
     }
   }
 };
-showPage(1, $student);
+
+let anchorTags = '<a href="#">' + pageNumber + "</a>";
+let pageAnchorTags = $("li").append(anchorTags);
+$("ul").after(pageAnchorTags);
+showPage(pageNumber, $student);
 // Create and append the pagination links - Creating a function that can do this is a good approach
 const appendPageLinks = () => {};
 
